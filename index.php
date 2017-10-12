4d04bc58be296909ded87e13119d58e33687e46b
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-12 16:56:35 -0600

    Added initial site overview

--- index.php
+++ index.php
@@ -12,11 +12,31 @@
 
 		<main>
 			<h2>Persona</h2>
+			<ul>
+				<li>17-24 years old.</li>
+				<li>Having difficulty connecting in a disconnected world.</li>
+				<li>Has trouble finding like minded individuals.</li>
+				<li>Needs some way to both find like minded individuals, and connect with them.</li>
+			</ul>
 
 			<h2>Use Case and Interaction Flow</h2>
+			<p>Assuming the user has already found a community they are interested in, and has decided
+			on a post they wish to make.</p>
+			<ol>
+				<li>User clicks a button to create a new post.</li>
+				<li>Site takes the user to a new post creation page.</li>
+				<li>User titles their post, adds some content, and clicks the submission button.</li>
+				<li>Site displays the users post at the top of the community, and returns the user to the
+				community page.</li>
+				<li>Another user sees the post, and wishes to comment.</li>
+				<li>User 2 clicks on the post.</li>
+				<li>Site takes user 2 to the page displaying the post.</li>
+				<li>User clicks a button to comment on the post.</li>
+				<li>Site takes user to a comment creation page.</li>
+				<li>User </li>
+			</ol>
 
 			<h2>Conceptual Model</h2>
-
 		</main>
 	</body>
 </html>
