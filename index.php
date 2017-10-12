f91e1a24a0776350c32c29ed3e9eb5d594c750a1
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-12 16:33:53 -0600

    Created index.php, and titles

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Data Design Project</title>
+	</head>
+
+	<body>
+		<main>
+			<header>
+				<h1>Data Design Project</h1>
+			</header>
+		</main>
+	</body>
+</html>
