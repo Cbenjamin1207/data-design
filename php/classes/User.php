2115863844ebdc8ee8b9f2cd6921e9618574b3f8
Calder Benjamin <calderbenjamin@gmail.com>
2017-10-20 11:50:25 -0600

    Created getUserByUserId function for user

--- php/classes/User.php
+++ php/classes/User.php
@@ -255,6 +255,49 @@ class User implements \JsonSerializable {
 	}
 
 	/**
+	 * gets the user by userId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param string $userId user id to search for
+	 * @return user|null user found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when a variable are not the correct data type
+	 **/
+	public static function getUserByUserId(\PDO $pdo, string $userId) : ?user {
+		// sanitize the tweetId before searching
+		try {
+			$userId = self::validateUuid($userId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		// create query template
+		$query = "SELECT userId, userEmail, userHash, userSalt,userName FROM `user` WHERE userId = :userId";
+		$statement = $pdo->prepare($query);
+
+		// bind the tweet id to the place holder in the template
+		$parameters = ["userId" => $userId->getBytes()];
+		$statement->execute($parameters);
+
+		// grab the tweet from mySQL
+		try {
+			$user = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$tweet = new User($row["userId"], $row["userEmail"], $row["userHash"], $row["userSalt"],
+					$row["userNamme"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return($user);
+	}
+
+
+
+	/**
 	 * formats the state variables for JSON serialization
 	 *
 	 * @return array resulting state variables to serialize
